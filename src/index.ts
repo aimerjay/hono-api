d1a15973c4278996db2cbe4ad92f5a99db5db5eb
Aimer Pedrozo <aimer.pedrozo@cambridge.org>
2025-08-13 18:07:22 +0800

    index modif

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ApiException, fromHono } from "chanfana";
 import { Hono } from "hono";
 import { tasksRouter } from "./endpoints/tasks/router";
+import { postRouter} from "./endpoints/posts/router";
 import { ContentfulStatusCode } from "hono/utils/http-status";
 import { DummyEndpoint } from "./endpoints/dummyEndpoint";
 
@@ -42,6 +43,8 @@ const openapi = fromHono(app, {
 
 // Register Tasks Sub router
 openapi.route("/tasks", tasksRouter);
+
+openapi.route("/posts", postRouter);
 
 // Register other endpoints
 openapi.post("/dummy/:slug", DummyEndpoint);
